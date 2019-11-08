bba11f74b2d39b47ef9ea72dd8d038ddaaae9449
Agis Anastasopoulos <a@xz0.org>
2019-11-08 10:32:16 +0200

    ...

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/agis/confluent-interceptor-segfault
+
+go 1.13
+
+require github.com/confluentinc/confluent-kafka-go v1.1.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <topic>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	fmt.Println(kafka.LibraryVersion())
+
+	broker := os.Args[1]
+	topic := os.Args[2]
+
+	p, err := kafka.NewProducer(
+		&kafka.ConfigMap{"bootstrap.servers": broker,
+			"plugin.library.paths": "monitoring-interceptor"})
+	if err != nil {
+		panic(err)
+	}
+
+	err = p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte("hi"),
+		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+	}, nil)
+
+	p.Close()
+}
